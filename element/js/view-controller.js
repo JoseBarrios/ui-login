739dc3f64835900269e5160bae22cc1057848eba
Jose Barrios <jose@cognilab.com>
2017-07-28 18:08:03 -0700

    basic attributes API

--- element/js/view-controller.js
+++ element/js/view-controller.js
@@ -5,33 +5,96 @@ let view = uiLoginDoc.ownerDocument.querySelector('#ui-login-view');
 
 class UILoginViewController extends HTMLElement{
 
+  static get observedAttributes() {
+    return ["action", "method", "registration", "password-reset", "error"];
+  }
+
   constructor(){
     super();
-  }
+    this._action = null;
+    this._method = null;
+    this._registration = null;
+    this._passwordReset = null;
+    this._error = null;
+ }
 
-  ///STANDARD
   connectedCallback() {
     console.log('connected');
-    //GET DOM STUFF HERE
-    //let shadowRoot = this.attachShadow({mode: 'open'});
-    //view = document.importNode(view.content, true);
-    //shadowRoot.appendChild(view);
-
     view = document.importNode(view.content, true);
     this.appendChild(view);
+
+    this.$form = this.querySelector('#form');
+    this.$csrf = this.querySelector('#csrf');
+    this.$registrationLink = this.querySelector('#registrationLink');
+    this.$passwordResetLink = this.querySelector('#passwordResetLink');
+    this.$error = this.querySelector('#error');
+
+    this.addEvents();
+   }
+
+  addEvents(){
+     this.$form.addEventListener('submit', this.submit);
   }
 
+  removeEvents(){
+    //REMOVE
+    //this.$form.addEventListener('submit', this.submit);
+  }
+
+  submit(){
+    e.preventDefault();
+    console.log('SUBMIT')
+  }
 
   disconnectedCallback() {
     console.log('disconnected');
   }
 
-  attributeChangedCallback(attrName, oldVal, newVal) {
-    console.log('attributeChanged');
+  attributeChangedCallback(attributeName, oldValue, newValue) {
+    console.log('attributeChanged', name, oldValue, newValue);
+    this[attributeName] = newValue;
   }
 
   adoptedCallback(){
     console.log('adoptedCallback');
+  }
+
+  get action(){ return this._action; }
+  set action(value){
+    if(this._action === value) return;
+    this._action = value;
+    this.setAttribute('action', value);
+    this.$form.action = value;
+  }
+
+  get method(){ return this._method; }
+  set method(value){
+    if(this._method === value) return;
+    this._method = value;
+    this.setAttribute('method', value);
+    this.$form.method = value;
+  }
+
+  get registration(){ return this._registration; }
+  set registration(value){
+    if(this._registration === value) return;
+    this._registration = value;
+    this.setAttribute('registration', value);
+  }
+
+  get passwordReset(){ return this._passwordReset; }
+  set passwordReset(value){
+    if(this._passwordReset === value) return;
+    this._passwordReset = value;
+    this.setAttribute('passwordReset', value);
+  }
+
+  get error(){ return this._error; }
+  set error(value){
+    if(this._error === value) return;
+    this._error = value;
+    this.setAttribute('error', value);
+    this.$error.visibility = 'visible';
   }
 }
 
